238f74d37e1a180c26bf9dbd61b79088f91326d8
darshanrahate <rahate.darshan@gmail.com>
2025-08-20 17:19:58 +0530

    Types of Attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { UserComponent } from './user/user.component';
+import { FooterComponent } from "./types_of_selectors/class_selector/footer/footer.component"; 
  
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [UserComponent],
+  imports: [UserComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'AngularProject_2';
