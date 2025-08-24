022098eac5921c8f6fd9eabb858ca361973b69aa
darshanrahate <rahate.darshan@gmail.com>
2025-08-24 08:05:37 +0530

    ngStyle Attribute Directive

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,30 @@
 import { Component } from "@angular/core";
-
+import { CommonModule } from '@angular/common';
 
 @Component({
     standalone: true,
     selector: 'app-user',
-    templateUrl: './user.component.html'
+    templateUrl: './user.component.html',
+    imports: [CommonModule]
 })
 export class UserComponent {
 
     userId: number = 10;
     userStatus: string = "Offline";
 
+    constructor() {
+        this.userStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getUserStatus() {
         return this.userStatus;
+    }
+
+    getColor() {
+        if (this.userStatus === 'online') {
+            return 'green';
+        }
+        return 'red';
     }
 
 }
